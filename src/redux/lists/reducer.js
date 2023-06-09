84bd39cd37473113321368d6eb796a49378be374
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-09 13:29:38 +0700

    modify reducer for list redux

--- src/redux/lists/reducer.js
+++ src/redux/lists/reducer.js
@@ -1,13 +1,10 @@
 import {
-  START_FETCHING_LISTS_CATEGORIES,
-  SUCCESS_FETCHING_LISTS_CATEGORIES,
-  ERROR_FETCHING_LISTS_CATEGORIES,
-  START_FETCHING_LISTS_TALENTS,
-  SUCCESS_FETCHING_LISTS_TALENTS,
-  ERROR_FETCHING_LISTS_TALENTS,
-  START_FETCHING_LISTS_EVENTS,
-  SUCCESS_FETCHING_LISTS_EVENTS,
-  ERROR_FETCHING_LISTS_EVENTS,
+  START_FETCHING_LISTS_JABATANS,
+  SUCCESS_FETCHING_LISTS_JABATANS,
+  ERROR_FETCHING_LISTS_JABATANS,
+  START_FETCHING_LISTS_SHIFTS,
+  SUCCESS_FETCHING_LISTS_SHIFTS,
+  ERROR_FETCHING_LISTS_SHIFTS,
 } from "./constants";
 
 const statuslist = {
@@ -18,53 +15,38 @@ const statuslist = {
 };
 
 const initialState = {
-  categories: [],
-  statusCategories: statuslist.idle,
-  talents: [],
-  statusTalents: statuslist.idle,
-  events: [],
-  statusEvents: statuslist.idle,
+  jabatans: [],
+  statusJabatans: statuslist.idle,
+  shifts: [],
+  statusShifts: statuslist.idle,
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case START_FETCHING_LISTS_CATEGORIES:
-      return { ...state, statusCategories: statuslist.process };
+    case START_FETCHING_LISTS_JABATANS:
+      return { ...state, statusJabatans: statuslist.process };
 
-    case ERROR_FETCHING_LISTS_CATEGORIES:
-      return { ...state, statusCategories: statuslist.error };
+    case ERROR_FETCHING_LISTS_JABATANS:
+      return { ...state, statusJabatans: statuslist.error };
 
-    case SUCCESS_FETCHING_LISTS_CATEGORIES:
+    case SUCCESS_FETCHING_LISTS_JABATANS:
       return {
         ...state,
-        statusCategories: statuslist.success,
-        categories: action.categories,
+        statusJabatans: statuslist.success,
+        jabatans: action.jabatans,
       };
 
-    case START_FETCHING_LISTS_TALENTS:
-      return { ...state, statusTalents: statuslist.process };
+    case START_FETCHING_LISTS_SHIFTS:
+      return { ...state, statusShifts: statuslist.process };
 
-    case ERROR_FETCHING_LISTS_TALENTS:
-      return { ...state, statusTalents: statuslist.error };
+    case ERROR_FETCHING_LISTS_SHIFTS:
+      return { ...state, statusShifts: statuslist.error };
 
-    case SUCCESS_FETCHING_LISTS_TALENTS:
+    case SUCCESS_FETCHING_LISTS_SHIFTS:
       return {
         ...state,
-        statusTalents: statuslist.success,
-        talents: action.talents,
-      };
-
-    case START_FETCHING_LISTS_EVENTS:
-      return { ...state, statusEvents: statuslist.process };
-
-    case ERROR_FETCHING_LISTS_EVENTS:
-      return { ...state, statusEvents: statuslist.error };
-
-    case SUCCESS_FETCHING_LISTS_EVENTS:
-      return {
-        ...state,
-        statusEvents: statuslist.success,
-        events: action.events,
+        statusShifts: statuslist.success,
+        shifts: action.shifts,
       };
 
     default:
