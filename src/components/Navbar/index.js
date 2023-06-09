5ec56771657c07c72cfadea2606d11928a7d62e7
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-09 13:27:45 +0700

    modify navbar components

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-// import NavLink from "../NavAccess";
-import { useNavigate, NavLink } from "react-router-dom";
-// import {
-//   accessCategories,
-//   accessTalents,
-//   accessEvents,
-//   accessParticipants,
-//   accessPayments,
-//   accessOrders,
-// } from "../../const/access";
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function PNavbar() {
   const navigate = useNavigate();
-  const [role, setRole] = useState(null);
-
-  // useEffect(() => {
-  //   const fetchData = () => {
-  //     let { role } = localStorage.getItem("auth")
-  //       ? JSON.parse(localStorage.getItem("auth"))
-  //       : {};
-
-  //     setRole(role);
-  //   };
-  //   fetchData();
-  // }, []);
 
   const handleLogout = () => {
     localStorage.clear();
@@ -35,70 +14,19 @@ export default function PNavbar() {
     <Navbar bg="light" variant="light" className="navbar">
       <Container>
         <Navbar.Brand href="#home">Dashboard</Navbar.Brand>
-        <div className="col-2">
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Search"
-            aria-label="Search"
-          />
-        </div>
         <Nav className="me-auto">
-          <NavDropdown title="KATEGORI" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to="/something" className={"text-decoration-none"}>
-                Something
-              </NavLink>
-            </NavDropdown.Item>
-          </NavDropdown>
-
-          <NavDropdown title="TEKNOLOGI" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to="/something" className={"text-decoration-none"}>
-                Something
-              </NavLink>
-            </NavDropdown.Item>
-          </NavDropdown>
-
-          <NavDropdown title="EXPLORE" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to="/something" className={"text-decoration-none"}>
-                Something
-              </NavLink>
-            </NavDropdown.Item>
-          </NavDropdown>
-
-          <NavDropdown title="PATNERSHIP" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <NavLink to="/something" className={"text-decoration-none"}>
-                Something
-              </NavLink>
-            </NavDropdown.Item>
-          </NavDropdown>
-
-          {/* <NavLink
-            role={role}
-            roles={accessEvents.lihat}
-            action={() => navigate("/events")}
+          <Nav.Link
+            className="text-decoration-none"
+            onClick={() => navigate("/karyawan")}
           >
-            Events
-          </NavLink> */}
+            Karyawan
+          </Nav.Link>
+          <Nav.Link
+            className="text-decoration-none"
+            onClick={() => navigate("/Jabatan")}
+          >
+            Jabatan
+          </Nav.Link>
         </Nav>
         <Nav className="justify-content-end">
           <Nav.Link onClick={() => handleLogout()}>Logout</Nav.Link>
