a38c91bae80aa9ea892dcc7448e8ceda684b5ba3
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-09 13:28:56 +0700

    modify reducer for auth redux

--- src/redux/auth/reducer.js
+++ src/redux/auth/reducer.js
@@ -2,17 +2,16 @@ import { USER_LOGIN, USER_LOGOUT } from "./constants";
 
 let initialState = localStorage.getItem("auth")
   ? JSON.parse(localStorage.getItem("auth"))
-  : { token: null, role: null };
+  : { token: null };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case USER_LOGIN:
       return {
         token: action.token,
-        role: action.role,
       };
     case USER_LOGOUT:
-      return { token: null, role: null };
+      return { token: null };
     default:
       return state;
   }
