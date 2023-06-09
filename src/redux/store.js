97123295be187d183543a2e2c3524a13d4577c89
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-09 13:28:44 +0700

    modify store redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,26 +7,18 @@ import {
 import thunk from "redux-thunk";
 
 import authReducer from "./auth/reducer";
-import categoriesReducer from "./categories/reducer";
 import notifReducer from "./notif/reducer";
-import talentsReducer from "./talents/reducer";
-import paymentsReducer from "./payments/reducer";
-import eventsReducer from "./events/reducer";
+import karyawansReducer from "./karyawans/reducer";
 import listsReducer from "./lists/reducer";
-import ordersReducer from "./orders/reducer";
 
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // global state
 const rootReducers = combineReducers({
   auth: authReducer,
-  categories: categoriesReducer,
   notif: notifReducer,
-  talents: talentsReducer,
-  payments: paymentsReducer,
-  events: eventsReducer,
+  karyawans: karyawansReducer,
   lists: listsReducer,
-  orders: ordersReducer,
 });
 const store = createStore(
   rootReducers,
