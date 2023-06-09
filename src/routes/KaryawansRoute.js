1f183a9becde7d981d6d7438b273e5095caefef5
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-09 18:06:18 +0700

    modify routes karyawan

--- src/routes/KaryawansRoute.js
+++ src/routes/KaryawansRoute.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
 import Karyawans from "../pages/karyawans";
+import Create from "../pages/karyawans/create";
 
 export function KaryawanRoute() {
   return (
     <Routes>
       <Route path="/" element={<Karyawans />} />
+      <Route path="/create" element={<Create />} />
     </Routes>
   );
 }
