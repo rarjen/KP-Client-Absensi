47d97a26bdf5254ef80f2ae62b47bb6749fbb53f
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-19 12:59:39 +0700

    modify redux actions

--- src/redux/lists/actions.js
+++ src/redux/lists/actions.js
@@ -5,6 +5,9 @@ import {
   START_FETCHING_LISTS_SHIFTS,
   ERROR_FETCHING_LISTS_SHIFTS,
   SUCCESS_FETCHING_LISTS_SHIFTS,
+  START_FETCHING_LISTS_KARYAWANS,
+  SUCCESS_FETCHING_LISTS_KARYAWANS,
+  ERROR_FETCHING_LISTS_KARYAWANS,
 } from "./constants";
 
 import { getData } from "../../utils/fetch";
@@ -12,6 +15,7 @@ import debounce from "debounce-promise";
 
 let debouncedFetchListsJabatans = debounce(getData, 1000);
 let debouncedFetchListsShifts = debounce(getData, 1000);
+let debouncedFetchListsKaryawans = debounce(getData, 1000);
 
 // Jabatan
 
@@ -62,6 +66,7 @@ export const fetchListJabatans = () => {
   };
 };
 
+// Shift
 export const startFetchingListsShifts = () => {
   return {
     type: START_FETCHING_LISTS_SHIFTS,
@@ -80,8 +85,6 @@ export const errorFetchingListShifts = () => {
     type: ERROR_FETCHING_LISTS_SHIFTS,
   };
 };
-
-// SHIFT
 
 export const fetchListShifts = () => {
   return async (dispatch) => {
@@ -107,6 +110,55 @@ export const fetchListShifts = () => {
       );
     } catch (error) {
       dispatch(errorFetchingListShifts());
+    }
+  };
+};
+
+// Karyawans
+
+export const startFetchingListsKaryawans = () => {
+  return {
+    type: START_FETCHING_LISTS_KARYAWANS,
+  };
+};
+
+export const successFetchingListKaryawans = ({ karyawans }) => {
+  return {
+    type: SUCCESS_FETCHING_LISTS_KARYAWANS,
+    karyawans,
+  };
+};
+
+export const errorFetchingListKaryawans = () => {
+  return {
+    type: ERROR_FETCHING_LISTS_KARYAWANS,
+  };
+};
+
+export const fetchListKaryawans = () => {
+  return async (dispatch) => {
+    dispatch(startFetchingListsKaryawans());
+
+    try {
+      let res = await debouncedFetchListsKaryawans("/karyawan");
+
+      let _temp = [];
+
+      res.data.data.forEach((res) => {
+        _temp.push({
+          value: res.id,
+          label: res.full_name,
+          target: { value: res.id, name: "karyawan" },
+        });
+      });
+
+      dispatch(
+        successFetchingListKaryawans({
+          karyawans: _temp,
+        })
+      );
+    } catch (error) {
+      dispatch(errorFetchingListKaryawans());
     }
   };
 };
