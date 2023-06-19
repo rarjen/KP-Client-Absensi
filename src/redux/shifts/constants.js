7062574725abda454a9901e119d615ad37de291d
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-06-19 13:02:06 +0700

    make constant redux for shift

---
+++ src/redux/shifts/constants.js
@@ -0,0 +1,3 @@
+export const START_FETCHING_SHIFT = "redux/shifts/START_FETCHING_SHIFT";
+export const SUCCESS_FETCHING_SHIFT = "redux/shifts/SUCCESS_FETCHING_SHIFT";
+export const ERROR_FETCHING_SHIFT = "redux/shifts/ERROR_FETCHING_SHIFT";
